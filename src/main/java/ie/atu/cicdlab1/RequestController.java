13ff6dc5e4b9975b806f8c0939ffe4224b6f6638
Antoj2000 <g00385306@atu.ie>
2024-10-01 11:07:53 +0100

    Greet by name

--- src/main/java/ie/atu/cicdlab1/RequestController.java
+++ src/main/java/ie/atu/cicdlab1/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.cicdlab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class RequestController {
         public String getMessage() { return "Some Message";}
     @GetMapping("/details")
         public String getDetails(){ return "Some other message";}
+
+    @GetMapping("/greet/{name}")
+        public String greetByName(@PathVariable String name) {
+            return "Hello, " + name + "!";
+    }
 
 }
