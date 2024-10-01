2081b784f600d8967e09e9545d5bfe18f4429fea
Antoj2000 <g00385306@atu.ie>
2024-10-01 11:51:24 +0100

    Calculator

--- src/main/java/ie/atu/cicdlab1/Addition.java
+++ src/main/java/ie/atu/cicdlab1/Addition.java
@@ -18,7 +18,10 @@ public class Addition {
         return "The multiple of these two numbers is " + sum;}
     @GetMapping("/divide")
     public String getDivision(@RequestParam int num1, int num2){
-
+        if (num2 == 0){
+            return "Cannot divise by 0";
+        }else {
         int sum = num1 / num2;
         return "The division is " + sum;}
+}
 }
