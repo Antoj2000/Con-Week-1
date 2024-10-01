1da1841394532f4a58e28082167e6d927bf7defc
Antoj2000 <g00385306@atu.ie>
2024-10-01 10:45:51 +0100

    Get Mapping

---
+++ src/main/java/ie/atu/cicdlab1/RequestController.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdlab1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+    @GetMapping("/message")
+        public String getMessage() { return "Some Message";}
+    @GetMapping("/details")
+        public String getDetails(){ return "Some other message";}
+
+}
