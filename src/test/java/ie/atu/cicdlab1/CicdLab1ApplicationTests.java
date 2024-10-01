25e9f1ee75830aef5826f191219eda0ba43eccf7
Antoj2000 <g00385306@atu.ie>
2024-10-01 10:20:01 +0100

    New project

---
+++ src/test/java/ie/atu/cicdlab1/CicdLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdlab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
