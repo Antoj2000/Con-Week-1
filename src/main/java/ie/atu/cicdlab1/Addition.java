f68a893912b200ef797bcab151439fbc8fd9d7c7
Antoj2000 <g00385306@atu.ie>
2024-10-07 12:16:06 +0100

    Added if statement for division error

--- src/main/java/ie/atu/cicdlab1/Addition.java
+++ src/main/java/ie/atu/cicdlab1/Addition.java
@@ -5,20 +5,30 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Addition {
     @GetMapping("/addition")
-    public String getAddition(@RequestParam int num1, int num2){
+    public String getAddition(@RequestParam int num1, int num2) {
         int sum = num1 + num2;
-        return "The sum of these two numbers is " + sum;}
-    @GetMapping("/subtract")
-    public String getSubtraction(@RequestParam int num1, int num2){
-        int sum = num1 - num2;
-        return "The answer is " + sum;}
-    @GetMapping("/multiply")
-    public String getMultiple(@RequestParam int num1, int num2){
-        int sum = num1 * num2;
-        return "The multiple of these two numbers is " + sum;}
-    @GetMapping("/divide")
-    public String getDivision(@RequestParam int num1, int num2){
+        return "The sum of these two numbers is " + sum;
+    }
 
-        int sum = num1 / num2;
-        return "The division is " + sum;}
-}
+    @GetMapping("/subtract")
+    public String getSubtraction(@RequestParam int num1, int num2) {
+        int sum = num1 - num2;
+        return "The answer is " + sum;
+    }
+
+    @GetMapping("/multiply")
+    public String getMultiple(@RequestParam int num1, int num2) {
+        int sum = num1 * num2;
+        return "The multiple of these two numbers is " + sum;
+    }
+
+    @GetMapping("/divide")
+    public String getDivision(@RequestParam int num1, int num2) {
+        if (num2 == 0) {
+            return "Cannot divide by 0";
+
+        } else {
+            int sum = num1 / num2;
+            return "The division is " + sum;
+        }
+}}
